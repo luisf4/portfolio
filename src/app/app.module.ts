f107b561dd9ae121728ff92565af961a8cbcf409
luisf4 <luismarcelino.dev@gmail.com>
2023-06-04 10:37:41 -0300

    feat: added contact section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BodyComponent } from './components/body/body.component';
 import { WellcomeComponent } from './components/content/wellcome/wellcome.component';
 import { ProjectsComponent } from './components/content/projects/projects.component';
 import { ShowcasesComponent } from './components/content/showcases/showcases.component';
+import { ContactComponent } from './components/content/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { ShowcasesComponent } from './components/content/showcases/showcases.com
     WellcomeComponent,
     ProjectsComponent,
     ShowcasesComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
